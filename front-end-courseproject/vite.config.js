b714bb07511849c4997a8eaa42d66b507c9f0128
ramon_deraes <ramon.de.raes@student.ehb.be>
2025-05-20 22:15:10 +0200

    pages 3.0

--- front-end-courseproject/vite.config.js
+++ front-end-courseproject/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CP-FRONT-END-RAMON337/"
-})
+  base: "/cp-front-end-ramon337/",
+});
